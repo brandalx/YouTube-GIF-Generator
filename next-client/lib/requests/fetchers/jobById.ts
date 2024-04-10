7aed569f99721b6ec1a5748ade7fb49e5a66bddf
brndalx <113251192+brandalx@users.noreply.github.com>
2024-04-10 15:42:19 -0400

    feat: fetchJobById fetcher

---
+++ next-client/lib/requests/fetchers/jobById.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import Job from "../../common/interfaces/Job.interface";
+
+export default async function fetchJobById(jobId: string): Promise<Job> {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/jobs/${jobId}`
+    );
+
+    return response.data;
+  } catch (err) {
+    //@ts-expect-error - no error
+    if (err.response?.status === 404) window.location.href = "/404";
+    throw err;
+  }
+}
